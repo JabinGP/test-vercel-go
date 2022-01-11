dcb88a3846cb74be9b0086a78524ac921b79e37b
JabinGP <1748511181@qq.com>
2022-01-11 13:19:01 +0800

    add embed json

--- api/search.go
+++ api/search.go
@@ -1,10 +1,29 @@
 package handler
 
 import (
+	"embed"
 	"fmt"
+	"json"
 	"net/http"
 )
 
+//go:embed site.json
+var f embed.FS
+
+var posts = []map[string]interface{}{}
+
+func init() {
+	data, _ := f.ReadFile("site.json")
+	err := json.Unmarshal(data, &posts)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
+	for _, post := range posts {
+		fmt.Fprintf(w, "<h1>%s</h1>", post["title"])
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JabinGP/test-vercel-go
+
+go 1.17
